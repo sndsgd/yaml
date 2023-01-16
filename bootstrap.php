be68ecad44326b3e5c9060db069e1428a5479ff6
sndsgd <r@snds.gd>
2023-01-16 12:32:56 -0500

    self review

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,8 @@
 <?php
 
+// this only exists for tests and static analysis. at the time it was
+// added phpstan could not discover constants created with `define()`.
+
 require __DIR__ . "/vendor/autoload.php";
 
 const TESTS_DIR = __DIR__ . "/tests";
--- tests/unit/constants.php
+++
@@ -1,3 +0,0 @@
-<?php declare(strict_types=1);
-
-define("TESTS_DIR", realpath(__DIR__ . "/.."));
